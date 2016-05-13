c07c8343b1272a38d3794cedba755a9f846f3bbd
Anand Gaitonde <AnandGaitonde@GMail.com>
2016-05-13 00:23:49 -0700

    list users via the 'users' command

--- commands/pt.go
+++ commands/pt.go
@@ -3,9 +3,10 @@ package commands
 type PTCommand struct {
 	TrackerURL string `default:"https://www.pivotaltracker.com/services/v5" long:"override-tracker-url" hidden:"true"`
 
-	AddUser    AddUserCommand    `command:"add-user" alias:"au" description:"Add user's API key and login"`
-	RemoveUser RemoveUserCommand `command:"remove-user" alias:"ru" description:"Remove user's API key"`
-	User       UserCommand       `command:"user" alias:"u" description:"Displays User"`
+	AddUser    AddUserCommand    `command:"add-user" alias:"au" description:"Add user's API token and login"`
+	RemoveUser RemoveUserCommand `command:"remove-user" alias:"ru" description:"Remove user's API token"`
+	User       UserCommand       `command:"user" alias:"u" description:"Displays pt user"`
+	Users      UsersCommand      `command:"users" description:"List all pt users"`
 }
 
 var PT PTCommand
---
+++ commands/users.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/goodmustache/pt/actions"
+)
+
+var ErrNoUsers = errors.New("No users have been added, run the 'add-users' command.")
+
+type UsersCommand struct{}
+
+func (cmd UsersCommand) Execute([]string) error {
+	config, err := actions.ReadConfig()
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrNoUsers
+		}
+
+		return err
+	} else if len(config.Users) == 0 {
+		return ErrNoUsers
+	}
+
+	var minimumColumnWidth int
+	for _, user := range config.Users {
+		if l := len(user.Name); l > minimumColumnWidth {
+			minimumColumnWidth = l
+		}
+
+		if l := len(user.Username); l > minimumColumnWidth {
+			minimumColumnWidth = l
+		}
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, minimumColumnWidth, 8, 2, '\t', 0)
+	fmt.Fprintln(w, "Name\tUsername\tAlias")
+
+	for _, user := range config.Users {
+		fmt.Fprintf(w, "%s\t%s\t%s\n", user.Name, user.Username, user.Alias)
+	}
+
+	w.Flush()
+
+	return nil
+}
