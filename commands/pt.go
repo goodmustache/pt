7b8bc0974b166337d099c0478c4c84d56b67cfb8
Anand Gaitonde <AnandGaitonde@GMail.com>
2016-05-13 01:24:58 -0700

    slash is required to do the URL join correctly

--- commands/pt.go
+++ commands/pt.go
@@ -1,7 +1,7 @@
 package commands
 
 type PTCommand struct {
-	TrackerURL string `default:"https://www.pivotaltracker.com/services/v5" long:"override-tracker-url" hidden:"true"`
+	TrackerURL string `default:"https://www.pivotaltracker.com/services/v5/" long:"override-tracker-url" hidden:"true"`
 
 	AddUser    AddUserCommand    `command:"add-user" alias:"au" description:"Add user's API token and login"`
 	RemoveUser RemoveUserCommand `command:"remove-user" alias:"ru" description:"Remove user's API token"`
