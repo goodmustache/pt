6b18d375d607961c961ffeb42bd29dfc01620455
Anand Gaitonde <AnandGaitonde@GMail.com>
2016-05-16 01:17:40 -0700

    godoc for config package

--- config/config.go
+++ config/config.go
@@ -5,20 +5,42 @@ import (
 	"time"
 )
 
+// Config holds metadata about pt's users
 type Config struct {
-	CurrentUserID      uint64    `json:"current_user_id"`
+	// CurrentUserID is the current user's ID
+	CurrentUserID uint64 `json:"current_user_id"`
+
+	// CurrentUserSetTime is when the current user is set
 	CurrentUserSetTime time.Time `json:"current_user_set_time"`
-	Users              []User    `json:"users"`
+
+	// Users is the list of pt's users
+	Users []User `json:"users"`
 }
 
+// User is a PT system user
 type User struct {
-	ID       uint64 `json:"id"`
+	// ID given from Tracker
+	ID uint64 `json:"id"`
+
+	// APIToken provided from the Tracker's settings page
 	APIToken string `json:"api_token"`
-	Name     string `json:"name"`
+
+	// Name is the real name of the user
+	Name string `json:"name"`
+
+	// Username for Tracker
 	Username string `json:"username"`
-	Alias    string `json:"alias,omitempty"`
+
+	// Alias setup by the user for quick reference
+	Alias string `json:"alias,omitempty"`
 }
 
+// AddUser adds users to config with the given credentials. Will error out if:
+// - API Token matches another user's API token (DuplicateAliasError)
+// - Username matches a saved alias (UsernameMatchesSavedAliasError)
+// - Alias matches another user's alias (AliasMatchesSavedUsernameError)
+// If a users user is added again (determined by the same ID), the original
+// user will be removed from the config.
 func (config *Config) AddUser(id uint64, apiToken string, name string, username string, alias string) error {
 	newUser := User{
 		ID:       id,
@@ -50,6 +72,8 @@ func (config *Config) AddUser(id uint64, apiToken string, name string, username 
 	return nil
 }
 
+// RemoveUser removes the given user from the config if it's ID matches one in
+// the config.
 func (config *Config) RemoveUser(userToRemove User) error {
 	var found bool
 	for i, user := range config.Users {
@@ -70,6 +94,7 @@ func (config *Config) RemoveUser(userToRemove User) error {
 	return nil
 }
 
+// SetCurrentUser sets the current user on the config.
 func (config *Config) SetCurrentUser(username string) error {
 	if username == "" {
 		config.CurrentUserID = 0
@@ -88,12 +113,14 @@ func (config *Config) SetCurrentUser(username string) error {
 	return ErrUserNotFound
 }
 
+// LoadConfig converts a config from a raw byte array.
 func LoadConfig(rawConfig []byte) (Config, error) {
 	var config Config
 	err := json.Unmarshal(rawConfig, &config)
 	return config, err
 }
 
+// SaveConfig converts a raw config into a byte array.
 func SaveConfig(config Config) ([]byte, error) {
 	return json.Marshal(config)
 }
--- config/errors.go
+++ config/errors.go
@@ -7,19 +7,29 @@ import (
 
 var ErrUserNotFound = errors.New("Specified user does not exist in config.")
 
+// DuplicateAliasError is the error that should be returned when adding a user
+// but the alias provided is the same as an existing pt user.
 type DuplicateAliasError struct {
+	// User is the pre-existing pt user
 	User User
 }
 
+// Error displays the error message
 func (e DuplicateAliasError) Error() string {
 	return fmt.Sprintf("Alias provided is already in use by %s (alias: %s)", e.User.Name, e.User.Alias)
 }
 
+// UsernameMatchesSavedAliasError is the error that should be returned when
+// adding a user but the username is the same as an existing user's alias.
 type UsernameMatchesSavedAliasError struct {
+	// SavedUser is the pre-existing pt user
 	SavedUser User
-	NewUser   User
+
+	// NewUser is the user to be added to pt
+	NewUser User
 }
 
+// Error displays the error message
 func (e UsernameMatchesSavedAliasError) Error() string {
 	return fmt.Sprintf(
 		"The user %s (%s) cannot be added because it's username matches %s (alias: %s)",
@@ -30,11 +40,17 @@ func (e UsernameMatchesSavedAliasError) Error() string {
 	)
 }
 
+// AliasMatchesSavedUsernameError is the error that should be returned when
+// adding a user but the alias is the same as an existing user's username.
 type AliasMatchesSavedUsernameError struct {
+	// SavedUser is the pre-existing pt user
 	SavedUser User
-	NewUser   User
+
+	// NewUser is the user to be added to pt
+	NewUser User
 }
 
+// Error displays the error message
 func (e AliasMatchesSavedUsernameError) Error() string {
 	return fmt.Sprintf(
 		"The user %s (alias: %s) cannot be added because it's alias matches %s (%s)",
